6439a18c25e79d36dd5dd5aa264128e96c8e5304
laroshan <laroshanlaro@gmail.com>
2021-08-25 21:20:07 +0530

    video remove

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,13 @@
 import React from "react";
 import HeroSection from "../Components/HeroSection";
-import video1 from "../videos/leolifevideo1.mp4";
+// import video1 from "../videos/leolifevideo1.mp4";
 import img1 from "../images/404.jpg";
 function Home() {
   return (
     <>
       <HeroSection
         isVid={true}
-        vid={video1}
+        // vid={video1}
         imgs={img1}
         title="Welcome To LD LeoLife"
         subtitle="Let's Do Shopping"
