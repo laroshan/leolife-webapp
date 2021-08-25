e77a868db228ee4a47c509c2457ec433f0d254e4
laroshan <laroshanlaro@gmail.com>
2021-08-25 17:37:12 +0530

    working

--- src/context/context.js
+++ src/context/context.js
@@ -3,6 +3,14 @@ import React, { Component } from "react";
 const ProductContext = React.createContext();
 
 class ProductProvider extends Component {
+  state = {
+    cart: [],
+    cartItms: 0,
+    cartTotal: 0,
+    storeProducts: [],
+    filteredProducts: [],
+    featuredProducts: [],
+  };
   render() {
     return (
       <ProductContext.Provider>{this.props.children}</ProductContext.Provider>
