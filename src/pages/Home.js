cd41192a3cf10cfacb44b6818b88de417c9ed054
laroshan <laroshanlaro@gmail.com>
2021-09-09 09:45:13 +0530

    commit 31

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import FeaturedProducts from "../Components/HomePage/FeaturedProducts";
 // import video1 from "../videos/Trim2.mp4";
 // import video2 from "../videos/leolifevideo1.mp4";
@@ -12,34 +12,37 @@ import BannerExample from "../Components/materialUi";
 function Home() {
   // const [mobile, Setmobile] = useState(false);
   // let width = window.innerWidth;
-  const [dimensions, setDimensions] = React.useState({
-    height: window.innerHeight,
-    width: window.innerWidth,
-  });
-  React.useEffect(() => {
-    function handleResize() {
-      setDimensions({
-        height: window.innerHeight,
-        width: window.innerWidth,
-      });
-    }
+  // const [dimensions, setDimensions] = React.useState({
+  //   height: window.innerHeight,
+  //   width: window.innerWidth,
+  // });
+  // React.useEffect(() => {
+  //   function handleResize() {
+  //     setDimensions({
+  //       height: window.innerHeight,
+  //       width: window.innerWidth,
+  //     });
+  //   }
 
-    window.addEventListener("resize", handleResize);
-  }, []);
+  //   window.addEventListener("resize", handleResize);
+  // }, []);
   // console.log(dimensions);
-  return dimensions.width > 960 ? (
+  return (
     <>
       <BannerExample />
 
       <FeaturedProducts />
     </>
-  ) : (
-    <>
-      <HeroSectionImg img={img1} max={true} mobImg={LeoLogo} />
-
-      <FeaturedProducts />
-    </>
   );
+  // dimensions.width > 960 ? (
+
+  // ) : (
+  //   <>
+  //     <HeroSectionImg img={img1} max={true} mobImg={LeoLogo} />
+
+  //     <FeaturedProducts />
+  //   </>
+  // );
 }
 
 export default Home;
